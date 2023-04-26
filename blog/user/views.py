6c015b7dc305497d259e38696cc3570a68e0a9d2
rexema <anast82@mail.ru>
2023-04-26 11:47:17 +0300

    views for user

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,24 +1,24 @@
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 from flask import Blueprint, redirect, render_template
 
+
+
 user_app = Blueprint('user_app', __name__, url_prefix='/users', static_folder='../static')
 
-USERS = {
-    1:{'name':"Анастасия",'surname': 'Соснина'},
-    2: {'name':"Мария",'surname': 'Иванова'},
-    3: {'name':"Елена",'surname': 'Шарапова'},
-
-}
 
 @user_app.route('/')
 def user_list():
-    return render_template('users/list.html', users=USERS)
+    from blog.models import User
+    users = User.query.all()
+    return render_template('users/list.html', users=users)
 
 
 @user_app.route('/<int:pk>')
+@login_required
 def get_user(pk:int):
-    try:
-        user = USERS[pk]
-    except KeyError:
-        return redirect('/users')
+    from blog.models import User
+    user = User.query.filter_by(id=pk).one_or_none()       
     return render_template('users/detail.html', user=user)
+
+
