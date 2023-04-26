df989c1cd319861b76db9840dfe401a20eac7d28
rexema <anast82@mail.ru>
2023-04-26 11:46:24 +0300

    views for auth

---
+++ blog/auth/views.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import logout_user, login_user, login_required
+from werkzeug.security import check_password_hash
+
+auth = Blueprint("auth", __name__, static_folder="../static")
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template(
+            "auth/login.html"
+        )
+    email = request.form.get("email")
+    password = request.form.get("password")
+    from blog.models import User
+
+    
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash("Check your login details")
+        return redirect(url_for(".login"))
+    login_user(user)
+    return redirect(url_for("user_app.get_user", pk=user.id))
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for(".login"))
