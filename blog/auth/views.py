fe8c1f4df8e589f731271f44899600ddefc33fec
Анастасия Соснина <rexema@gmail.com>
2023-05-12 17:43:16 +0500

    updated view for login

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -2,27 +2,27 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import logout_user, login_user, login_required
 from werkzeug.security import check_password_hash
 
+from blog.forms.auth import UserAuthForm
+
 auth = Blueprint("auth", __name__, static_folder="../static")
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    form = UserAuthForm(request.form)
+    from blog.models import User
     if request.method == "GET":
-        return render_template(
-            "auth/login.html"
-        )
+
+        return render_template("auth/login.html", form=form)
     email = request.form.get("email")
     password = request.form.get("password")
-    from blog.models import User
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(email=email).first()
+        if not user and check_password_hash(user.password, password):
+            return render_template("auth/login.html", form=form)
 
-    
-    user = User.query.filter_by(email=email).first()
-
-    if not user or not check_password_hash(user.password, password):
-        flash("Check your login details")
-        return redirect(url_for(".login"))
-    login_user(user)
-    return redirect(url_for("user_app.get_user", pk=user.id))
+        login_user(user)
+        return redirect(url_for("user_app.get_user", pk=user.id))
 
 
 @auth.route("/logout")
