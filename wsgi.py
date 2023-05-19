b2fa155658508e160dab4396fdad986570ffdb81
Анастасия Соснина <rexema@gmail.com>
2023-05-19 17:10:49 +0500

    added command to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,40 @@
-from blog.app import create_app, db, migrate
-from werkzeug.security import generate_password_hash
+import click
+from blog.app import create_app
+
 
 app = create_app()
-
-
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         debug=True,
 
     )
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    from blog.app import db
+    db.create_all()
+    print("done!")
+
+@app.cli.command("create-tags")
+def create_tags():
+
+    from blog.models import Tag
+    from blog.app import db
+    for name in [
+    "flask",
+    "django",
+    "python",
+    "sqlalchemy",
+    "news",
+    ]:
+        tag = Tag(name=name)
+        db.session.add(tag)
+    db.session.commit()
+    print("created tags")
 
