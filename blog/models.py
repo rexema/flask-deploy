a828c691c024a55cb9fefcf11afbc31b95022d97
Анастасия Соснина <rexema@gmail.com>
2023-05-25 15:38:17 +0500

    some changes in models

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ from flask_admin import Admin
 article_tag_associations_table = Table(
     'article_tag_associations',
     db.metadata,
-    db.Column('article_id', db.Integer, db.ForeignKey('article.id'), nullable=False),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'),nullable=False)
+    db.Column('article_id', db.Integer, db.ForeignKey(
+        'article.id'), nullable=False),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), nullable=False)
 )
 
 
@@ -21,12 +22,18 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     author = db.relationship("Author", uselist=False, back_populates="user")
 
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     user = db.relationship("User", back_populates="author")
     articles = db.relationship('Article', back_populates='author')
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
 
 
 class Article(db.Model):
@@ -35,11 +42,19 @@ class Article(db.Model):
     text = db.Column(db.Text, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     author = db.relationship("Author", back_populates="articles")
-    tags = db.relationship("Tag", secondary=article_tag_associations_table, back_populates='articles')
+    tags = db.relationship(
+        "Tag", secondary=article_tag_associations_table, back_populates='articles')
+
+    def __str__(self):
+        return self.title
+
 
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False, default="", server_default="")
-    articles = db.relationship('Article',secondary=article_tag_associations_table, back_populates='tags')
+    name = db.Column(db.String(255), nullable=False,
+                     default="", server_default="")
+    articles = db.relationship(
+        'Article', secondary=article_tag_associations_table, back_populates='tags')
 
-
+    def __str__(self):
+        return self.name
