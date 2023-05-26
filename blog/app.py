f0f11db7d754cedcefea6eff6824b500e764803a
Анастасия Соснина <rexema@gmail.com>
2023-05-26 15:59:21 +0500

    app.py

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_combo_jsonapi import Api
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from blog.user.views import user_app
@@ -10,11 +11,29 @@ import os
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
+from combojsonapi.spec import ApiSpecPlugin
+from flask_combo_jsonapi import Api
 
 migrate = Migrate()
 db = SQLAlchemy()
 login_manager = LoginManager()
+api = Api()
+
+
+def create_api_spec_plugin(app):
+    api_spec_plugin = ApiSpecPlugin(
+        app=app,
+        # Declaring tags list with their descriptions,
+        # so API gets organized into groups. it's optional.
+        tags={
+            "Tag": "Tag API",
+            "User": "User API",
+            "Author": "Author API",
+            "Article": "Article API",
+
+        }
+    )
+    return api_spec_plugin
 
 
 def create_app():
@@ -23,6 +42,7 @@ def create_app():
     app.config.from_object(f'blog.config.{cfg_name}')
     register_extensions(app)
     register_blueprints(app)
+    register_api(app)
     from blog.admin import admin
     admin.init_app(app)
 
@@ -56,3 +76,21 @@ def register_commands(app: Flask):
     from wsgi import init_db, create_tags
     app.cli.add_command(init_db)
     app.cli.add_command(create_tags)
+
+
+def register_api(app: Flask):
+    from blog.api.tag import TagList, TagDetail
+    from blog.api.user import UserList, UserDetail
+    from blog.api.author import AuthorList, AuthorDetail
+    from blog.api.article import ArticleList, ArticleDetail
+    api = Api(app=app, plugins=[create_api_spec_plugin(app),])
+    api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
+    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
+    api.route(UserList, "user_list", "/api/users/", tag="User")
+    api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
+    api.route(AuthorList, "author_list", "/api/authors/", tag="Author")
+    api.route(AuthorDetail, "author_detail",
+              "/api/authors/<int:id>/", tag="Author")
+    api.route(ArticleList, "article_list", "/api/articles/", tag="Article")
+    api.route(ArticleDetail, "article_detail",
+              "/api/articles/<int:id>/", tag="Article")
