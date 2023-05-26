77e6c1ac986d17f87b8be0e1aa8e79b0e98dd4f5
Анастасия Соснина <rexema@gmail.com>
2023-05-26 16:01:42 +0500

    init for schemas

---
+++ blog/schemas/__init__.py
@@ -0,0 +1,12 @@
+from blog.schemas.tag import TagSchema
+from blog.schemas.article import ArticleSchema
+from blog.schemas.author import AuthorSchema
+from blog.schemas.user import UserSchema
+
+__all__ = [
+    'TagSchema',
+    "UserSchema",
+    "AuthorSchema",
+    "ArticleSchema",
+
+]
