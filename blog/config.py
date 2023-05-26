353bd618f3bd0567a49c35318a288edb8688ba41
Анастасия Соснина <rexema@gmail.com>
2023-05-26 15:59:50 +0500

    add swagger to config

--- blog/config.py
+++ blog/config.py
@@ -9,6 +9,9 @@ class BaseConfig(object):
     SECRET_KEY = 'e+%_r$h4aui+vw_jn^!3)&pk6v=i-3!&dcy07i6x@97gf#-osc'
     WTF_CSRF_ENABLED = True
     FLASK_ADMIN_SWATCH = 'cosmo'
+    OPENAPI_URL_PREFIX = '/api/swagger'
+    OPENAPI_SWAGGER_UI_PATH = '/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
 
 
 class DevConfig(BaseConfig):
