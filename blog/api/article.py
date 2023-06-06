e67eb4efd9ec1183a6520ca8ac2173f0ba2ef745
Анастасия Соснина <rexema@gmail.com>
2023-06-06 09:17:42 +0500

    added events to article.py

--- blog/api/article.py
+++ blog/api/article.py
@@ -2,10 +2,23 @@ from flask_combo_jsonapi import ResourceDetail, ResourceList
 from blog.schemas import ArticleSchema
 from blog.app import db
 from blog.models import Article
+from combojsonapi.event.resource import EventsResource
+
+
+class ArticleListEvents(EventsResource):
+    def event_get_count(self):
+        return {"count": Article.query.count()}
+
+   
+
+class ArticleDetailEvents(EventsResource):
+    def event_get_count_by_author(self,  **kwargs):
+        return {'count': Article.query.filter(Article.author_id== kwargs["id"]).count()}
 
 
 class ArticleList(ResourceList):
     schema = ArticleSchema
+    events = ArticleListEvents
     data_layer = {
         "session": db.session,
         "model": Article,
@@ -14,7 +27,12 @@ class ArticleList(ResourceList):
 
 class ArticleDetail(ResourceDetail):
     schema = ArticleSchema
+    events = ArticleDetailEvents
     data_layer = {
         "session": db.session,
         "model": Article,
     }
+
+class ArticleListEvents(EventsResource):
+    def event_get_count(self):
+        return {"count": Article.query.count()}
