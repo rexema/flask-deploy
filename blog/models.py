d8300104f8df135088190efdf57377f3c649a7e1
Анастасия Соснина <rexema@gmail.com>
2023-06-06 09:17:22 +0500

    models

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from flask_login import UserMixin
 from sqlalchemy import Table
 from blog.app import db
 from flask_admin import Admin
-
+from werkzeug.security import check_password_hash
 
 article_tag_associations_table = Table(
     'article_tag_associations',
@@ -22,18 +22,23 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     author = db.relationship("Author", uselist=False, back_populates="user")
 
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password, password)
+
     def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
-
-
+        return self.email
+    
+   
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     user = db.relationship("User", back_populates="author")
     articles = db.relationship('Article', back_populates='author')
 
+    
     def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
+        return self.user.email
+
 
 
 class Article(db.Model):
@@ -42,8 +47,7 @@ class Article(db.Model):
     text = db.Column(db.Text, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     author = db.relationship("Author", back_populates="articles")
-    tags = db.relationship(
-        "Tag", secondary=article_tag_associations_table, back_populates='articles')
+    tags = db.relationship("Tag", secondary=article_tag_associations_table, back_populates='articles')
 
     def __str__(self):
         return self.title
@@ -53,8 +57,7 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False,
                      default="", server_default="")
-    articles = db.relationship(
-        'Article', secondary=article_tag_associations_table, back_populates='tags')
+    articles = db.relationship('Article', secondary=article_tag_associations_table, back_populates='tags')
 
     def __str__(self):
         return self.name
